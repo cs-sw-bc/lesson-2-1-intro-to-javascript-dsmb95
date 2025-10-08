31ce35aba88b6f687dedab6d50b0471959afc38d
dsmb95 <daniellesamanthamananquil@gmail.com>
2025-10-07 17:49:30 -0700

    completed instructions
    completed the instructions for the day

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+// Exteranl JavaScript
+let c = a + b;
+
+let name = 'Sam';
+name = "Sam's sam";
+name = 'sam"s sam';
+
+let eligibility = false; // BOOLEAN
+
+//Do not use reserved names as a variable name.
+let bug = null; // you can put null as value
+
+// Use camelCasing when writing variables in JavaScript.
+// You can't leave any spaces when writing variable names.
+let firstName = 'Danielle';
+let lastName = 'Mananquil';
+
+//Console log - display message for testing.
+console.log(`My full name is ${firstName} ${lastName}.`);
+console.log(typeof(lastName))
+
+// When you combing a string and a number, one with print out as string and the other is number.
+let x = "2";
+let y = 1;
+let z = x + y;
+
+// Variable Naming
+let name1; // allowed
+let _name; // allowed
+// let 1name - NOT ALLOWED
+
+//CONSTANTS - always type in uppercase!
+const PI = 3.14159; // SCREEMING CAPITAL CASE
+const GRAVITY_OF_EARTH = 9.8; // SCREEMING_SNAKE_CASE
+
+let radius = 2;
+let area = PI * (radius**2);
+
+let modulo = 37 % 10; //remainder of the division operator.
+modulo = c % a;
+
+let age = 24;
+eligibility = age > 18; // is 24 greater than 18? = yes so it's TRUE.
